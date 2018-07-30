28766a3cb4d7003e66c69fa6b095ef334c4b0d04
bhagyaj <bhagyajayashani@gmail.com>
2018-07-30 14:31:20 +0530

    discovery added

--- src/main/java/com/bhagya/ServiceMain.java
+++ src/main/java/com/bhagya/ServiceMain.java
@@ -2,8 +2,10 @@ package com.bhagya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(ServiceMain.class,args);
